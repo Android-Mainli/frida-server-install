c123de9e1066e82af5d8b3ae07044e12f7b825e4
lixiaoliang04 <lixiaoliang04@lixiaoliang-MBP-2.local>
2023-03-14 23:34:20 +0800

    v0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # frida-server-install
+
+- 把frida-server-16.0.11-android-arm64添加至环境变量
+- 移动证书功能
--- customize.sh
+++ customize.sh
@@ -1,9 +1,9 @@
-#声明SKIPUNZIP=1 完全自主安装
-SKIPUNZIP=1
-
 ui_print "开始解压"
-unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
-mv -f $MODPATH/system/binpath $MODPATH/system/xbin
+unzip -o "$ZIPFILE" -x 'META-INF/*' -x 'README.md' -x '.gitignore' -x '.git/*' -d $MODPATH >&2
+# 创建证书目录
+mkdir -p $MODPATH/system/etc/security/cacerts
+set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm $MODPATH/system/xbin/frida-server-16.0.11-android-arm64 0 0 0755
-ls -R $MODPATH
+set_perm $MODPATH/post-fs-data.sh 0 0 0755
+ls -lR $MODPATH
 ui_print "安装完成"
